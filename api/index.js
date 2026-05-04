249af7253451b9db3c4d475dd4429d2e0aa7a01f
Omar Saad <omarsaad@microsoft.com>
2026-05-04 17:13:43 +0300

    Add root Node entry file for Functions worker compatibility

---
+++ api/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Some Node workers on Flex Consumption look for the package.json entry point.
+// Function triggers are still defined via function.json in each function folder.
+module.exports = {};
